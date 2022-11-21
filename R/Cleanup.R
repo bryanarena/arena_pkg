5f54f839dbc9e969f59f952db38ad6079016a1f0
Bryan Arena <Bryan.arena@selu.edu>
2022-11-21 18:07:45 +0000

    3rd function

---
+++ R/Cleanup.R
@@ -0,0 +1,17 @@
+#' Removes any NAs from a given dataset
+#' 
+#' @param filepath - ability to insert data
+#' 
+#' @return A dataset with no lingering NA values present
+#' 
+#' @examples 
+clean_up <- function(filepath){
+  data <- read_csv(filepath)
+  clean_up <- data %>% 
+    drop_na()
+  if (sum(is.na(clean_up)) == 0){
+    return(clean_up)
+  } else {
+    print("NAs still present!")
+  }
+} 
